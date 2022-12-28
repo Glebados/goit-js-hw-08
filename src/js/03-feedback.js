bfe646e682928c92e01337fbd49f11c0e234c340
Glebados <glebrudenko22@gmail.com>
2022-12-28 20:08:38 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const formEL = document.querySelector('.feedback-form');
-const emailEl = document.querySelector('[name="email"]');
-const messageEl = document.querySelector('[name="message"]');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
@@ -12,15 +12,20 @@ formEL.addEventListener('submit', onSubmitForm);
 formEL.addEventListener('input', throttle(saveForm, 1000));
 
 function saveForm(e) {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  }
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(event) {
   event.preventDefault();
-  event.target.reset();
+
   console.log('Storage:', JSON.parse(localStorage.getItem(STORAGE_KEY)));
   localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  formData = {};
 }
 
 function loadForm() {
